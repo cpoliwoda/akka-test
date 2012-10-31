56f0eec7bc501c8ac008acfafeb7dcd4ae17f6b7
Michael Hoffer <info@michaelhoffer.de>
2012-10-31 11:08:05 +0100

    sample working (main1 & main2)

--- src/test/actors/remote/Main1.java
+++ src/test/actors/remote/Main1.java
@@ -17,80 +17,69 @@ import com.typesafe.config.ConfigFactory;
 public class Main1 {
 
     final static String actorsystemname = "MySystem";
-    final static String hostname = "141.2.38.37";
-    final static String port = "2552";//"1099";
+    final static String hostname = "127.0.0.1";
+    final static String port = "2552"; //"2552";//"1099";
     final static String deployActor = "/greeter";
     final static String actorpath = "user" + deployActor;
-    
-    final static String configString = "    akka {\n"
-                + "      actor {\n"
-                + "        provider = \"akka.remote.RemoteActorRefProvider\"\n"
-                + "      }\n"
-                + "      deployment {\n"
-                //                + "            default {\n"
-                //                + "                remote = \""+actorpath+"\"\n" 
-                //                + "            }\n"
-                + "             " + deployActor + " {\n"
-                + "                 remote = \"akka://" + actorsystemname + "@" + hostname + ":" + port +"\"\n"
-                + "             }"
-                + "      }\n"
-                + "    }";
 
     public static void main(String[] args) {
 
-        Config config = ConfigFactory.parseString(configString);
+        String actorName = "greeter";
+        String systemName = "sys1";
 
-        ActorSystem system = ActorSystem.create(actorsystemname, config);
+        Config config = ConfigFactory.parseString(Configs.createConfig("127.0.0.1", 2552, systemName, actorName));
 
+        ActorSystem system = ActorSystem.create(systemName, config);
+        ActorRef greeter = system.actorOf(new Props(GreetingActor.class), actorName);
     }
 }
 
 /*  JAVA EXAMPLE FROM: http://akka.io/#remoting
 
 
-    // ------------------------------
-    // config on all machines
-    akka {
-    actor {
-    provider = akka.remote.RemoteActorRefProvider
-    deployment {
-    /greeter {
-    remote = akka://MySystem@machine1:2552
-    }
-    }
-    }
-    }
+ // ------------------------------
+ // config on all machines
+ akka {
+ actor {
+ provider = akka.remote.RemoteActorRefProvider
+ deployment {
+ /greeter {
+ remote = akka://MySystem@machine1:2552
+ }
+ }
+ }
+ }
      
-    // ------------------------------
-    // define the greeting actor and the greeting message
-    public class Greeting implements Serializable {
-    public final String who;
-    public Greeting(String who) { this.who = who; }
-    }
+ // ------------------------------
+ // define the greeting actor and the greeting message
+ public class Greeting implements Serializable {
+ public final String who;
+ public Greeting(String who) { this.who = who; }
+ }
      
-    public class GreetingActor extends UntypedActor {
-    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+ public class GreetingActor extends UntypedActor {
+ LoggingAdapter log = Logging.getLogger(getContext().system(), this);
      
-    public void onReceive(Object message) throws Exception {
-    if (message instanceof Greeting)
-    log.info("Hello " + ((Greeting) message).who);
-    }
-    }
+ public void onReceive(Object message) throws Exception {
+ if (message instanceof Greeting)
+ log.info("Hello " + ((Greeting) message).who);
+ }
+ }
      
-    // ------------------------------
-    // on machine 1: empty system, target for deployment from machine 2
-    ActorSystem system = ActorSystem.create("MySystem");
+ // ------------------------------
+ // on machine 1: empty system, target for deployment from machine 2
+ ActorSystem system = ActorSystem.create("MySystem");
      
-    // ------------------------------
-    // on machine 2: Remote Deployment - deploying on machine1
-    ActorSystem system = ActorSystem.create("MySystem");
-    ActorRef greeter = system.actorOf(new Props(GreetingActor.class), "greeter");
+ // ------------------------------
+ // on machine 2: Remote Deployment - deploying on machine1
+ ActorSystem system = ActorSystem.create("MySystem");
+ ActorRef greeter = system.actorOf(new Props(GreetingActor.class), "greeter");
      
-    // ------------------------------
-    // on machine 3: Remote Lookup (logical home of “greeter” is machine2, remote deployment is transparent)
-    ActorSystem system = ActorSystem.create("MySystem");
-    ActorRef greeter = system.actorFor("MySystem@machine2:2552/user/greeter");
-    greeter.tell(new Greeting("Sonny Rollins"));
+ // ------------------------------
+ // on machine 3: Remote Lookup (logical home of “greeter” is machine2, remote deployment is transparent)
+ ActorSystem system = ActorSystem.create("MySystem");
+ ActorRef greeter = system.actorFor("MySystem@machine2:2552/user/greeter");
+ greeter.tell(new Greeting("Sonny Rollins"));
 
 
-*/
+ */
--- src/test/actors/remote/Main2.java
+++ src/test/actors/remote/Main2.java
@@ -16,65 +16,70 @@ import com.typesafe.config.ConfigFactory;
  */
 public class Main2 {
 
-    
-
     public static void main(String[] args) {
 
-        Config config = ConfigFactory.parseString(Main1.configString);
+        String actorName = "greeter";
+        String systemName = "sys2";
 
-        ActorSystem system = ActorSystem.create(Main1.actorsystemname, config);
+        Config config = ConfigFactory.parseString(Configs.createConfig("127.0.0.1", 2553, null, null));
 
-        ActorRef greeter = system.actorOf(new Props(GreetingActor.class), "greeter");
+        ActorSystem system = ActorSystem.create(systemName, config);
 
+        // REMOTE
+        //    akka://<actorsystemname>@<hostname>:<port>/<actor path>
+        ActorRef greeter = system.actorFor("akka://" + "sys1"
+                + "@" + Main1.hostname + ":" + 2552 + "/user/" + actorName);
+
+        greeter.tell(new Greeting("Charlie Parker"));
     }
 }
 
 /*  JAVA EXAMPLE FROM: http://akka.io/#remoting
 
 
-    // ------------------------------
-    // config on all machines
-    akka {
-    actor {
-    provider = akka.remote.RemoteActorRefProvider
-    deployment {
-    /greeter {
-    remote = akka://MySystem@machine1:2552
-    }
-    }
-    }
-    }
+ // ------------------------------
+ // config on all machines
+ akka {
+ actor {
+ provider = akka.remote.RemoteActorRefProvider
+ deployment {
+ /greeter {
+ remote = akka://MySystem@machine1:2552
+ }
+ }
+ }
+ }
      
-    // ------------------------------
-    // define the greeting actor and the greeting message
-    public class Greeting implements Serializable {
-    public final String who;
-    public Greeting(String who) { this.who = who; }
-    }
+ // ------------------------------
+ // define the greeting actor and the greeting message
+ public class Greeting implements Serializable {
+ public final String who;
+ public Greeting(String who) { this.who = who; }
+ }
      
-    public class GreetingActor extends UntypedActor {
-    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+ public class GreetingActor extends UntypedActor {
+ LoggingAdapter log = Logging.getLogger(getContext().system(), this);
      
-    public void onReceive(Object message) throws Exception {
-    if (message instanceof Greeting)
-    log.info("Hello " + ((Greeting) message).who);
-    }
-    }
+ public void onReceive(Object message) throws Exception {
+ if (message instanceof Greeting)
+ log.info("Hello " + ((Greeting) message).who);
+ }
+ }
      
-    // ------------------------------
-    // on machine 1: empty system, target for deployment from machine 2
-    ActorSystem system = ActorSystem.create("MySystem");
+ // ------------------------------
+ // on machine 1: empty system, target for deployment from machine 2
+ ActorSystem system = ActorSystem.create("MySystem");
      
-    // ------------------------------
-    // on machine 2: Remote Deployment - deploying on machine1
-    ActorSystem system = ActorSystem.create("MySystem");
-    ActorRef greeter = system.actorOf(new Props(GreetingActor.class), "greeter");
+ // ------------------------------
+ // on machine 2: Remote Deployment - deploying on machine1
+ ActorSystem system = ActorSystem.create("MySystem");
+ ActorRef greeter = system.actorOf(new Props(GreetingActor.class), "greeter");
      
-    // ------------------------------
-    // on machine 3: Remote Lookup (logical home of “greeter” is machine2, remote deployment is transparent)
-    ActorSystem system = ActorSystem.create("MySystem");
-    ActorRef greeter = system.actorFor("MySystem@machine2:2552/user/greeter");
-    greeter.tell(new Greeting("Sonny Rollins"));
+ // ------------------------------
+ // on machine 3: Remote Lookup (logical home of “greeter” is machine2, remote deployment is transparent)
+ ActorSystem system = ActorSystem.create("MySystem");
+ ActorRef greeter = system.actorFor("MySystem@machine2:2552/user/greeter");
+ greeter.tell(new Greeting("Sonny Rollins"));
 
 
-*/
+ */
